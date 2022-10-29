c6807aca3035e3bd56cc4d1c29233c9b5499f75a
unisanta <unisanta@santacecilia.br>
2022-10-28 22:04:44 -0300

    atualiza

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-document.getElementById("saida").textContent = "Oi!";
+const nome = document.getElementById("nome");
+const btnSaudar = document.getElementById("btnSaudar");
+const saida = document.getElementById("saida");
+
+btnSaudar.onclick = function(){
+    saida.textContent ="Oi, " + nome.value + "!";
+}
+
+//document.getElementById("saida").textContent = "Oi!";
